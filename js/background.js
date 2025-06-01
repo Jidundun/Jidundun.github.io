db7311f78ce78c0187068397096cf60505bd5bfc
JIYOON <151109776+Jidundun@users.noreply.github.com>
2025-06-02 02:09:11 +0900

    Update background.js

--- js/background.js
+++ js/background.js
@@ -3,6 +3,6 @@ const images = ["0.jpeg", "1.jpeg", "2.jpeg"]
 const randomimage = images[Math.floor(Math.random()* images.length)];
 
 const bgImage = document.createElement("img");
-bgImage.src = `../images/${randomimage}`;
+bgImage.src = `Images/${randomimage}`;
 bgImage.classList.add("bg-image");
 document.body.appendChild(bgImage);
